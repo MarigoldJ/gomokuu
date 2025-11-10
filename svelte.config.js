253b57ff2dbe2494db4f08759af2011216b9bde4
Yusang Jeon <yusang0130@gmail.com>
2025-11-10 15:15:28 +0900

    Add GitHub Pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: process.env.GITHUB_ACTIONS
+        ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''}`
+        : ''
+    },
+    prerender: {
+      default: true
+    }
   },
   preprocess: [vitePreprocess({
     postcss: true
